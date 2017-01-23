80ad9003ac287d2386f0cb49837b42e0af0af9c1
lloricode <emorickfighter@gmail.com>
2017-01-23 19:15:56 +0800

    update html errors

--- application/config/admin/admin_config.php
+++ application/config/admin/admin_config.php
@@ -9,38 +9,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | main name sa website/company
   | ---------------------------------
   |
-  | 
+  |
   |
   |
   |
  */
 $config['admin_title'] = '<span>SMCL</span>Apartelle';
 
-/*
-  | ---------------------------------
-  | footer information
-  | ---------------------------------
-  |
-  |
-  |
-  |
-  |
- */
-$config['address'] = 'Labason Zambo';
-$config['contact_number'] = '+63912 345 678';
-$config['email'] = 'admin@admin.com';
-
-
-/*
-  | ---------------------------------
-  | other
-  | ---------------------------------
-  |
-  |
-  |
-  |
-  |
- */
-
-$config['current_year'] = '2017';
-$config['view_folder'] = 'public';
